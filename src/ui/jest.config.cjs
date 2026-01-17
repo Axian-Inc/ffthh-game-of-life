9779af3138b8bf708a1f77ba2a95facc6619dda5
Chad Riddle <criddle@axian.com>
2026-01-17 04:39:17 +0000

    Add Jest coverage reporting and thresholds

--- src/ui/jest.config.cjs
+++ src/ui/jest.config.cjs
@@ -1,6 +1,23 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['text', 'text-summary', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 85,
+      functions: 85,
+      lines: 85,
+      statements: 85,
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!src/**/*.test.{js,jsx}',
+    '!src/**/test-utils/**',
+    '!src/**/setupTests.js',
+    '!src/index.jsx',
+  ],
   moduleFileExtensions: ['js', 'jsx'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
