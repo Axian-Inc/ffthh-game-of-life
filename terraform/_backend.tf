4398010a107fb755d2d991b274acf4f7cf0f0681
Terry Griffin <9451944+griffint61@users.noreply.github.com>
2026-03-04 11:28:38 -0800

    Fixed dynamodb deprecation warning by switching to S3 locking for the backend.

--- terraform/_backend.tf
+++ terraform/_backend.tf
@@ -1,9 +1,11 @@
 terraform {
+  required_version = "~> 1.10"
+
   backend "s3" {
     bucket         = "ffthh-game-of-life-tf-state"
     key            = "envs/terraform.tfstate"
     region         = "us-west-2"
-    dynamodb_table = "ffthh-game-of-life-tf-lock"
+    use_lockfile   = true
     encrypt        = true
   }
 }
